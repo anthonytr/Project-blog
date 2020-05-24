159dc89b1344f6dcc3892fb7727ec2a003e251ce
Anthony <anthony.trillo.92@gmail.com>
2020-05-23 22:43:01 -0300

    Completed relationships

--- app/Post.php
+++ app/Post.php
@@ -14,8 +14,23 @@ class Post extends Model
      *
      * @return array
      */
+
+    protected $fillable = [
+        'user_id', 'category_id', 'name', 'slug', 'excerpt', 'body', 'status', 'file'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
     public function tags()
     {
-        return $this->morphToMany('App\Tag', 'taggable');
+        return $this->belongsToMany('App\Tag');
     }
 }
--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,7 +12,7 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         factory(App\Post::class, 300)->create()->each(function(App\Post $post){
-            $post->tags()->attack([
+            $post->tags()->attach([
                 rand(1,5),
                 rand(6,14),
                 rand(15,20),
